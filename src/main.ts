7f491507e8dfcafd501364575544f0d6acddc152
Yogeshp0012 <yogeshp0012@gmail.com>
2024-07-06 14:43:12 +0530

    Fix Cors

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        origin: [
-            'http://localhost:4200',
-            'https://readnswap.vercel.app/',
-        ],
-        methods: ["GET", "POST"],
-        credentials: true,
-    });
+    const app = await NestFactory.create(AppModule, { cors: true });
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
